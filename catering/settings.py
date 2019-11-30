63ab80ae9d0e4b13b3fa2690784721db166b0630
Wesley Jin <wesleyjin12@berkeley.edu>
2019-11-30 11:41:52 -0800

    Add django-heroku module & Procfile

--- catering/settings.py
+++ catering/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +126,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Activate Django-Heroku.
+# https://github.com/heroku/django-heroku
+django_heroku.settings(locals())
