bb10bd1c125c9efe2900e47674424ed019256427
Wesley Jin <wesleyjin12@berkeley.edu>
2021-02-22 20:47:56 -0800

    Update order mgmt models

--- order_management/models.py
+++ order_management/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
 
 # Create your models here.
 
@@ -9,7 +11,7 @@ class Item(models.Model):
 	item_name = models.CharField(max_length=30)
 	item_type = models.CharField(max_length=30)
 	price = models.DecimalField(max_digits=38, decimal_places=2)
-	size = models.CharField(max_length=10, blank=True)
+	size = models.CharField(max_length=64, blank=True)
 	
 	def __unicode__(self):
 		return self.item_type + " - " + self.item_name
@@ -21,14 +23,18 @@ class Item(models.Model):
 class Menu(models.Model):
 	menu_id = models.AutoField(primary_key=True)
 	menu_name = models.CharField(max_length=30)
-	menu_date = models.DateField
+	menu_date = models.DateField(default=date.today())
 	items = models.ManyToManyField(Item)
 
 	def __unicode__(self):
 		return self.menu_name
 	
 	def __str__(self):
-		return self.__unicode__()		
+		return self.__unicode__()
+	
+	def get_absolute_url(self):
+		return reverse("menu_detail", kwargs={"pk": self.pk})
+	
 
 
 class Ingredient(models.Model):
@@ -96,19 +102,54 @@ class Order(models.Model):
 	order_date = models.DateField()
 	deliver_date = models.DateField()
 	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
-	items = models.ManyToManyField(Item,
-		through='OrderItem',
-		through_fields=('order', 'item'))
-	total_sales = models.DecimalField(max_digits=38, decimal_places=2)
-	is_delivery = models.BooleanField(blank=True, null=True)
-	paid = models.BooleanField(blank=True, null=True)
+	total_sales = models.DecimalField(max_digits=38, decimal_places=2, blank=True, null=True)
+	discount = models.DecimalField(max_digits=38, decimal_places=2, default=0.0)
+	net_sales = models.DecimalField(max_digits=38, decimal_places=2, blank=True, null=True)
+	is_delivery = models.BooleanField(default=True)
+	completed = models.BooleanField(default=False)
 	last_updated = models.DateTimeField(auto_now=True)
 	created_date = models.DateTimeField(auto_now_add=True)
 
+	class Meta:
+		ordering = ["-order_id"]
 
-class OrderItem(models.Model):
+	def __str__(self):
+		return f"Order #{self.order_id} [{self.order_date}, {self.customer}]"
+
+	def save(self, *args, **kwargs):
+		self.total_sales = self.get_total_sales
+		self.net_sales = self.get_net_sales
+		super().save(*args, **kwargs)
+
+	@property
+	def get_total_sales(self):
+		return LineItem.objects.filter(order=self.order_id).aggregate(models.Sum('subtotal'))["subtotal__sum"]
+
+	@property
+	def get_net_sales(self):
+		if self.total_sales:
+			return self.total_sales - self.discount
+		else:
+			return - self.discount
+
+	@property
+	def get_num_items(self):
+		return LineItem.objects.filter(order=self.order_id).aggregate(models.Sum('quantity'))['quantity__sum']
+
+
+
+class LineItem(models.Model):
 	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
 	item = models.ForeignKey(Item, on_delete=models.SET_NULL, null=True)
 	quantity = models.IntegerField()
-	cost = models.DecimalField(max_digits=38, decimal_places=2)
+	subtotal = models.DecimalField(max_digits=38, decimal_places=2, null=True)
 	packaging = models.ForeignKey(Packaging, on_delete=models.SET_NULL, blank=True, null=True)
+
+	@property
+	def get_subtotal(self):
+		return self.item.price * self.quantity
+	
+	def save(self, *args, **kwargs):
+		self.subtotal = self.get_subtotal
+		super(LineItem, self).save(*args, **kwargs)
+	
